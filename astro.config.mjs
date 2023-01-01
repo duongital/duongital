31fa343605d48da4fb9588bc85bd4e3ca5aaaaab
Duong Nguyen <duongital@gmail.com>
2023-01-01 11:59:36 +0700

    feat: add ggl analytics

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,9 +2,14 @@ import { defineConfig } from 'astro/config';
 import mdx from '@astrojs/mdx';
 
 import sitemap from '@astrojs/sitemap';
+import partytown from '@astrojs/partytown';
 
 // https://astro.build/config
 export default defineConfig({
 	site: 'https://duongital.com',
-	integrations: [mdx(), sitemap()],
+	integrations: [mdx(), sitemap(), partytown({
+		config: { 
+			forward: ["dataLayer.push"] 
+		  },
+	})],
 });
