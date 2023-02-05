d227711f34d232c4493e2131b06b5c6cff2bc7d4
Duong Nguyen <duongital@gmail.com>
2023-02-05 22:27:55 +0700

    chore: change route from vn to vi

--- src/config.ts
+++ src/config.ts
@@ -2,4 +2,4 @@
 // You can import this data from anywhere in your site by using the `import` keyword.
 
 export const SITE_TITLE = '@duongital';
-export const SITE_DESCRIPTION = 'Welcome to my website!';
+export const SITE_DESCRIPTION = 'frontend dev, indie hacker and love teaching';
