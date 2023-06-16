73783ca375f27c90eb14d10a91d20c6b66c21e31
Duong Nguyen <duongital@gmail.com>
2023-06-16 22:57:45 +0700

    feat: add tailwindcss

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,19 +1,16 @@
-import { defineConfig } from 'astro/config'
-import mdx from '@astrojs/mdx'
+import { defineConfig } from 'astro/config';
+import mdx from '@astrojs/mdx';
+import sitemap from '@astrojs/sitemap';
+import partytown from '@astrojs/partytown';
 
-import sitemap from '@astrojs/sitemap'
-import partytown from '@astrojs/partytown'
+import tailwind from "@astrojs/tailwind";
 
 // https://astro.build/config
 export default defineConfig({
   site: 'https://duongital.com',
-  integrations: [
-    mdx(),
-    sitemap(),
-    partytown({
-      config: {
-        forward: ['dataLayer.push'],
-      },
-    }),
-  ],
-})
+  integrations: [mdx(), sitemap(), partytown({
+    config: {
+      forward: ['dataLayer.push']
+    }
+  }), tailwind()]
+});
