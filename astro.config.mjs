de86afb732857ebbf0f1768253020c570d71c340
duongital <duongital@gmail.com>
2024-01-24 18:46:27 +0700

    feat: upgrade to astro v4

--- astro.config.mjs
+++ astro.config.mjs
@@ -7,6 +7,9 @@ import tailwind from "@astrojs/tailwind";
 
 // https://astro.build/config
 export default defineConfig({
+  vite: {
+    base: ""
+  },
   site: 'https://duongital.com',
   integrations: [mdx(), sitemap(), partytown({
     config: {
